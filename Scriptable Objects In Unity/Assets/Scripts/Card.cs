9244ba80427a9719d3e7db9a51dcdbc192f5b1cc
Bin <jimmy28year@gmail.com>
2018-01-11 11:02:13 -0800

    Update20180111

---
+++ Scriptable Objects In Unity/Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+    public string description;
+    public int mana;
+    public int attack;
+    public int life;
+
+    public string CardToPrint()
+    {
+        string result = "name:" + name + "| description:" + description + "|mana:" + mana + "|attack:" + attack + "|life" + life;
+        return result;
+    }
+}
---
+++ Scriptable Objects In Unity/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+    public Text description;
+    public Text mana;
+    public Text attack;
+    public Text life;
+    Image img;
+
+    // Use this for initialization
+    void Start()
+    {
+        img = GetComponent<Image>();
+
+        //Debug.Log(card.CardToPrint());
+        description.text = card.description;
+        mana.text = card.mana.ToString();
+        attack.text = card.attack.ToString();
+        life.text = card.life.ToString();
+        img.sprite = card.sprite;
+
+
+    }
+}
